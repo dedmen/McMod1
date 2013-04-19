54caacd61963c566ac2368beb68b41ec0a08448e
Dedmen Miller <dedmenmiller@googlemail.com>
2013-04-19 12:13:14 +0200

    Added init Scripts

---
+++ common/com/dedmen/MyMod1/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.dedmen.MyMod1.lib;
+
+public class Reference {
+	public static final String MOD_ID = "DM1";
+	public static final String MOD_NAME = "Dedmens MC Mod";
+	public static final String VERSION = "0.0.1";
+
+}
